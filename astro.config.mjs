5f05f48f21dd99b59b5d4feaa4e8a3e97e3a9a2f
toddmath <tmatheson11186@gmail.com>
2023-09-18 16:20:43 -0700

    fix netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,14 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 
-import netlify from "@astrojs/netlify/edge-functions";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [prefetch(), sitemap(), tailwind(), partytown()],
-  adapter: netlify(),
+  adapter: netlify({
+    edgeMiddleware: true,
+    // functionPerRoute: true,
+  }),
   output: "hybrid",
-  // adapter: netlify({
-  //   edgeMiddleware: true
-  // })
 });
